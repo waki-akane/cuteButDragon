c96ca62337665b10ec67e954230ff1675af0ba9c
3030707 <3030707@30307-07>
2024-07-02 09:58:26 +0900

    Merge branch 'master' of https://github.com/waki-akane/cuteButDragon.git

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,14 +1,7 @@
 package com.example.demo.controller;
 
-import org.springframework.ui.Model;
 
 public class UserController {
 	
-	
-	
-	public String addUser(Model model) {
-		
-		return "user";
-	}
 
 }
