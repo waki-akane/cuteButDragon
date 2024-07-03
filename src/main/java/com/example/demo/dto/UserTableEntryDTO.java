0f5c9adc364095aca65ae59f9ea3a0a7712fd55e
3030676 <3030676@30306-76>
2024-07-03 11:35:31 +0900

    Merge branch 'master' of https://github.com/waki-akane/cuteButDragon.git

--- src/main/java/com/example/demo/dto/UserTableEntryDTO.java
+++ src/main/java/com/example/demo/dto/UserTableEntryDTO.java
@@ -2,20 +2,21 @@ package com.example.demo.dto;
 
 import java.io.Serializable;
 
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
+
 public class UserTableEntryDTO implements Serializable {
 	@Size(max = 10, message = "1～10文字のひらがな")
+	@NotEmpty(message = "名前を入力してください。")
 	private String name;
 	
-	 @Pattern(regexp = "^[a-zA-Z0-9]{8,16}$", message = "8～16文字の半角英数字")
+	 @Pattern(regexp = "^[a-zA-Z0-9]{8,16}$", message = "半角英数字のみ使用可")
+	 @Size(min=6,max = 16,message = "6～16文字")
+	 @NotEmpty(message = "パスワードを入力してください。")
 	private String pass;
 
 }
--- src/main/java/com/example/demo/dto/UserTableLoginDTO.java
+++ src/main/java/com/example/demo/dto/UserTableLoginDTO.java
@@ -1,19 +1,15 @@
 package com.example.demo.dto;
 
-import jakarta.validation.constraints.Pattern;
-import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class UserTableLoginDTO {
-	@Size(max = 10, message = "1～10文字のひらがなを入力してください")
+	@NotEmpty(message = "名前を入力してください。")
 	private String name;
 	
-	 @Pattern(regexp = "^[a-zA-Z0-9]{8,16}$", message = "8～16文字の半角英数字を入力してください")
+	@NotNull(message = "パスワードを入力してください。")
 	private String pass;
 
 }
