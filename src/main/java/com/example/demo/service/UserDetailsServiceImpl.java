3a209b9c169ccdac4e31b1ace4c40fecff28d819
3030676 <3030676@30306-76>
2024-07-03 15:44:00 +0900

    セキュリティ

--- src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
+++ src/main/java/com/example/demo/service/UserDetailsServiceImpl.java
@@ -1,5 +1,39 @@
 package com.example.demo.service;
 
-public class UserDetailsServiceImpl {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.demo.repository.UserTableRepository;
+import com.example.demo.service.userdetails.UserDetailsImpl;
+
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	UserTableRepository userTableRepository;
+
+	@Override
+	public UserDetails loadUserByUserId(Integer userId) throws UsernameNotFoundException {
+		
+		//Optional<UserTableEntity>loginUser = userTableRepository.findById(pass);
+		//Collection<GrantedAuthority> authorities = new ArrayList<>();
+		 try {
+	            String sql = "SELECT * FROM usertable WHERE user_name = ?";
+	            Map<String, Object> map = .queryForMap(sql, username);
+	            String password = (String)map.get("pass");
+	            Collection<GrantedAuthority> authorities = new ArrayList<>();
+	            authorities.add(new SimpleGrantedAuthority((String)map.get("authority")));
+	            return new UserDetailsImpl(username, password, authorities);
+	        } catch (Exception e) {
+	            throw new UsernameNotFoundException("ユーザーが見つかりません", e);
+	        }
+	}
 
 }
--- src/main/java/com/example/demo/service/userdetails/UserDetailsImpl.java
+++ src/main/java/com/example/demo/service/userdetails/UserDetailsImpl.java
@@ -1,5 +1,39 @@
 package com.example.demo.service.userdetails;
 
-public class UserDetailsImpl {
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.example.demo.entity.UserTableEntity;
+
+public class UserDetailsImpl implements UserDetails {
+	
+	private final UserTableEntity userTableEntity;
+
+	private Collection<GrantedAuthority> authorities;
+	
+	public UserDetailsImpl(UserTableEntity userTableEntity,Collection<GrantedAuthority> authorities) {
+		this.userTableEntity = userTableEntity;
+		this.authorities = authorities;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+
+		return userTableEntity.getPass();
+	}
+
+	@Override
+	public String getUsername() {
+		
+		return userTableEntity.getUserName();
+	}
 
 }
