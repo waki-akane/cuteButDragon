0a16699b54157f9f641266959f36c2607677b196
3030707 <3030707@30307-07>
2024-07-04 11:51:02 +0900

    imのサービスやら追加

--- src/main/java/com/example/demo/service/InitialMonsterServiceImpl.java
+++ src/main/java/com/example/demo/service/InitialMonsterServiceImpl.java
@@ -1,24 +1,31 @@
 package com.example.demo.service;
 
 import java.util.List;
+import java.util.Optional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.entity.InitialMonsterEntity;
+import com.example.demo.repository.InitialMonsterRepository;
 
 @Service
 public class InitialMonsterServiceImpl implements InitialMonsterService{
+	
+	@Autowired
+	InitialMonsterRepository imr;
 
 	@Override
 	public List<InitialMonsterEntity> showAllIm() {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		List<InitialMonsterEntity> list = imr.findAll();
+		return list;
 	}
 
 	@Override
 	public InitialMonsterEntity showIm(int imId) {
-		// TODO 自動生成されたメソッド・スタブ
-		return null;
+		Optional<InitialMonsterEntity> op = imr.findById(imId);
+		InitialMonsterEntity im = op.get();
+		return im;
 	}
 
 
