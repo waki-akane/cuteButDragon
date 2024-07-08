3e6744c11165e2dcc84875bcf0595f6317b548e9
3030707 <3030707@30307-07>
2024-07-08 15:07:50 +0900

    aaa

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,13 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.dto.UserTableEntryDTO;
+import com.example.demo.service.UsertableService;
 import com.example.demo.service.userdetails.UserDetailsImpl;
 
 public class UserController {
+	
+	@Autowired
+	UsertableService uts;
+	
 	@GetMapping("/login")
 	public String loginForm() {
 		// ログイン画面を表示
@@ -26,10 +33,19 @@ public class UserController {
 	}
 	
 	// SecurityConfig の failureUrl で指定した URL と?のうしろのパラメータ
-		@GetMapping(value="/login", params="failure")
-		public String loginFail(Model model) {
-			model.addAttribute("failureMessage", "ログインに失敗しました");
-			// ログイン画面を表示
-			return "login";
-		}
+	@GetMapping(value="/login", params="failure")
+	public String loginFail(Model model) {
+		model.addAttribute("failureMessage", "ログインに失敗しました");
+		// ログイン画面を表示
+		return "login";
+	}
+	
+	@GetMapping("addUser")
+	public String addUser(UserTableEntryDTO ute) {
+		uts.clearUser(ute);
+		return "new";
+	}
+	
+	
+		
 }
