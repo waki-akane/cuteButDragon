17cf6d51e7cd2ffb8561c9830852ae3fcfd20d1a
3030707 <3030707@30307-07>
2024-07-08 15:00:18 +0900

    urrentMerge branch 'master' of https://github.com/waki-akane/cuteButDragon.git

--- src/main/java/com/example/demo/service/UsertableServiceImpl.java
+++ src/main/java/com/example/demo/service/UsertableServiceImpl.java
@@ -1,16 +1,26 @@
 package com.example.demo.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.example.demo.dto.UserTableEntryDTO;
 import com.example.demo.entity.UserTableEntity;
+import com.example.demo.repository.UserTableRepository;
 
 @Service
 public class UsertableServiceImpl implements UsertableService{
+	
+	@Autowired
+	UserTableRepository utr;
 
 	@Override
 	public int createUser(UserTableEntryDTO userTableDTO) {
-		// TODO 自動生成されたメソッド・スタブ
+		UserTableEntity ute = new UserTableEntity();
+		ute.setUserName(userTableDTO.getName());
+		String pass = new BCryptPasswordEncoder().encode(userTableDTO.getPass());
+		ute.setPass(pass);
+		utr.save(ute);
 		return 0;
 	}
 
