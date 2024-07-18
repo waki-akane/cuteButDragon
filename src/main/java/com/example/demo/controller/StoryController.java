a49d08ac4b9b0daab351d55f01079a31d0780c00
3030707 <3030707@30307-07>
2024-07-18 11:46:52 +0900

    Revert "html,css"
    This reverts commit 78fdf7d84d6356552943fcc401b7b43f497a05f4.

---
+++ src/main/java/com/example/demo/controller/StoryController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StoryController {
+	
+	public String toCharacter() {
+		
+		return "character";
+	}
+
+}
