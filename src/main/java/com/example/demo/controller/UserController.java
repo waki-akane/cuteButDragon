78fdf7d84d6356552943fcc401b7b43f497a05f4
3031027 <3031027@30310-27>
2024-07-18 10:44:02 +0900

    html,css

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -14,16 +14,16 @@ import com.example.demo.service.userdetails.UserDetailsImpl;
 
 @Controller
 public class UserController {
-	
+
 	@Autowired
 	UsertableService uts;
-	
+
 	@GetMapping("/login")
 	public String loginForm() {
 		// ログイン画面を表示
 		return "login";
 	}
-	
+
 	@GetMapping("loginsuccess")
 	public String loginSuccess(Model model) {
 		// ユーザー名
@@ -32,30 +32,28 @@ public class UserController {
 		model.addAttribute("username", principal.getUsername());
 		// ログインに成功したら表示する URL
 		return "stage";
-		
+
 	}
-	
+
 	// SecurityConfig の failureUrl で指定した URL と?のうしろのパラメータ
-	@GetMapping(value="/login", params="failure")
+	@GetMapping(value = "/login", params = "failure")
 	public String loginFail(Model model) {
 		model.addAttribute("failureMessage", "ログインに失敗しました");
 		// ログイン画面を表示
 		return "login";
 	}
-	
-	@PostMapping("/register")
-	public String addUser(UserTableEntryDTO ute,Model model) {
+
+	@PostMapping("/user")
+	public String addUser(UserTableEntryDTO ute, Model model) {
 		uts.createUser(ute);
-		model.addAttribute("user", new UserTableEntryDTO()); 
+		model.addAttribute("user", new UserTableEntryDTO());
 		return "redirect:login";
 	}
-	
-	@GetMapping("/register")
+
+	@GetMapping("/user")
 	public String showForm(Model model) {
-		model.addAttribute("user",new UserTableEntryDTO());
-		return "register";
+		model.addAttribute("user", new UserTableEntryDTO());
+		return "user";
 	}
-	
-	
-		
+
 }
