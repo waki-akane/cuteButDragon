6db9b6d29da64e2d09d12651478b2758f68da634
3030707 <3030707@30307-07>
2024-08-05 10:47:46 +0900

    エラーメッセージが表示されないバグの修正

--- src/main/java/com/example/demo/dto/UserTableEntryDTO.java
+++ src/main/java/com/example/demo/dto/UserTableEntryDTO.java
@@ -11,10 +11,10 @@ import lombok.Data;
 public class UserTableEntryDTO implements Serializable {
 	@Size(max = 10, message = "最大文字数10文字を超えています。")
 	@NotEmpty(message = "名前を入力してください。")
-	@Pattern(regexp = "^[//p{InHiragana}ー]+$", message = "ひらがなで入力してください。")
+	@Pattern(regexp = "^[\\u3040-\\u309F]+$", message = "ひらがなで入力してください。")
 	private String name;
 	
-	 @Pattern(regexp = "^[a-zA-Z0-9]{8,16}$", message = "半角英数字で入力してください。")
+	 @Pattern(regexp = "^[a-zA-Z0-9]+$", message = "半角英数字で入力してください。")
 	 @Size(min=6,max = 16,message = "6～16文字で入力してください。")
 	 @NotEmpty(message = "パスワードを入力してください。")
 	 private String pass;
